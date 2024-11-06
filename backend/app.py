698ff715f050902d4a948f1bc733b13927207343
Sergio <sergiotutu2812@gmail.com>
2024-11-06 18:29:47 -0500

    Importacion de todas las vistas

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from backend import create_app
 from backend.modelos.modelos import Proveedor, Categoria, Subcategoria, Producto, Fecha_Registro_Prod, Rol, Usuario, Factura, Venta, Detalle_Venta
 from .modelos import db
+from flask_restful import Api
+from .vistas.vista_proveedor import VistaProveedor, VistaProveedored
+from .vistas.vista_categoria import VistaCategoria, VistaCategoriaed
+from .vistas.vista_subcategoria import VistaSubcategoria, VistaSubcategoriaed
+from .vistas.vista_productos import VistaProducto, VistaProductoed
+from .vistas.vista_usuarios import VistaUsuario, VistaUsuarioed
+from .vistas.vista_venta import VistaVenta, VistaVentaed
 
 app = create_app('default')
 app_context = app.app_context()
