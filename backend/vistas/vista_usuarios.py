5a88d5af2d11eae9f2d2a7acabc11fb56ab57711
Sergio <sergiotutu2812@gmail.com>
2024-11-07 07:22:23 -0500

    Agregando y modificando las vistas de usuarios

--- backend/vistas/vista_usuarios.py
+++ backend/vistas/vista_usuarios.py
@@ -1,4 +1,5 @@
 from flask import request
+from datetime import datetime
 from flask_restful import Resource
 from ..modelos import db, Usuario, UsuarioSchema
 
@@ -11,11 +12,13 @@ class VistaUsuario(Resource):
         return [usuario_schema.dump(Usuario) for Usuario in Usuario.query.all()]
 
     def post(self):
+        fecha = request.json['fecha_inicio_contrato']
+        fecha_inicio_contrato = datetime.strptime(fecha, "%Y-%m-%d").date()
         nuevo_usuario = Usuario(nombre_usu = request.json ['nombre_usu'],
                                 contrasena_usu = request.json['contrasena_usu'],    
                                 email_usu = request.json['email_usu'],    
                                 telefono_usu = request.json['telefono_usu'],
-                                fecha_inicio_contrato = request.json['fecha_inicio_contrato'],
+                                fecha_inicio_contrato = fecha_inicio_contrato,
                                 cedula_usu = request.json['cedula_usu'],
                                 rol = request.json['rol'])
         db.session.add(nuevo_usuario)
@@ -34,7 +37,9 @@ class VistaUsuarioed(Resource):
         usuario.contrasena_usu = request.json.get('contrasena_usu', usuario.contrasena_usu)
         usuario.email_usu = request.json.get('email_usu', usuario.email_usu)
         usuario.telefono_usu = request.json.get('telefono_usu', usuario.telefono_usu)
-        usuario.fecha_inicio_contrato = request.json.get('fecha_inicio_contrato', usuario.fecha_inicio_contrato)
+        if 'fecha_inicio_contrato' in request.json:
+            fecha = request.json['fecha_inicio_contrato']
+            usuario.fecha_inicio_contrato = datetime.strptime(fecha, "%Y-%m-%d").date()
         usuario.cedula_usu = request.json.get('cedula_usu', usuario.cedula_usu)
         usuario.rol = request.json.get('rol', usuario.rol)
 
