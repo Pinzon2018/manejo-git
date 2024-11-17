05804cd195db46b232bad56030cd35549c683f35
zukurama el pro <Sergio Pinzon>
2024-11-17 18:28:30 -0500

    Modificando el archivo gitignore

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'sergio9123541'
+    app.config['JWT_SECRET_KEY'] = 'pinzon1234133'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proyecto_bellayactual.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['FLASK_RUN_PORT'] = 5001
